edd9224202cc31470f01dc6d56876aee10200046
ragoolaman <ragoolaman01@gmail.com>
2016-01-16 19:08:17 -0700

    Finished game!

--- KFTextBox.java
+++ KFTextBox.java
@@ -27,6 +27,12 @@ public class KFTextBox extends JPanel implements ActionListener {
     public static String gameOver = "Congratulations on making it this far. Unfortunately, the Mad King Isratosh the Gluttonous proved to be a match for you. Play again and see how far you can get." + newline;
     public static boolean bobQuest = true;
 	public static boolean initial = false;
+	public static boolean bobQ = false;
+	public static boolean michealQ = false;
+	public static boolean gabriellaQ = false;
+	public static boolean thomasQ = false;
+	public static boolean raguTalk1 = false;
+	public static boolean finalQA = false;
     public KFTextBox() {
         super(new GridBagLayout());
         textField = new JTextField(20);
@@ -333,7 +339,14 @@ public class KFTextBox extends JPanel implements ActionListener {
         if (text.contains("/getcuroom")) 
         {
         	curRoom();
-        	System.out.println(curRoom); //DEBUG
+        	textField.selectAll();
+        } else
+        if(text.contains("/doquest")) 
+        {
+        	bobQ = true;
+        	michealQ = true;
+        	gabriellaQ = true;
+        	thomasQ = true;
         	textField.selectAll();
         } else
 //talk to char
@@ -347,8 +360,74 @@ public class KFTextBox extends JPanel implements ActionListener {
     			textField.selectAll();
     			break;
     		case "MainHall":
-    			textArea.append(MainHall.getSpecCharacter(toTalk).talkTo() + newline);
-    			textField.selectAll();
+    			switch(toTalk) 
+    			{
+    			case "ragoolaman":
+    				if(!raguTalk1) 
+    				{
+    					textArea.append(MainHall.getSpecCharacter(toTalk).talkTo() + newline);
+    					textField.selectAll();
+    					raguTalk1 = true;
+    				} else
+    				if(bobQ && michealQ && gabriellaQ && thomasQ) 
+    				{
+    					textArea.append("I see you did those jobs I asked about. Well, I have bob's key with me here. Micheal has arranged for ursurper guards to be there. Gabriella has given you the support of the people. And finally Thomas has spread discontent with the king among the remaining guards." + newline);
+    					textArea.append("Take care of any remaining business you have. Come back and accept my quest when you are ready to begin." + newline);
+    					textArea.append("Just remember that when you are finished my job, you will become the Feller of Kingdoms. I think the title will suit you well." + newline);
+    					finalQA = true;
+    				} else
+    				{
+    					textArea.append("You need to complete your four jobs before you can talk to Ragoolaman again!" + newline);
+    				}
+    				break;
+    			case "bob":
+    				if(!bobQ) 
+    				{
+    	    			textArea.append(MainHall.getSpecCharacter(toTalk).talkTo() + newline);
+    	    			textField.selectAll();
+    				} else 
+    				{
+    					textArea.append("Bob has left the hall to make your key." + newline);
+    					textField.selectAll();
+    				}
+    				break;
+    			case "micheal":
+    				if(!michealQ) 
+    				{
+    	    			textArea.append(MainHall.getSpecCharacter(toTalk).talkTo() + newline);
+    	    			textField.selectAll();
+    				} else 
+    				{
+    					textArea.append("Micheal has left the hall. Perhaps to sharpen his knife." + newline);
+    					textField.selectAll();
+    				}
+    				break;
+    			case "gabriella":
+    				if(!gabriellaQ) 
+    				{
+    	    			textArea.append(MainHall.getSpecCharacter(toTalk).talkTo() + newline);
+    	    			textField.selectAll();
+    				} else 
+    				{
+    					textArea.append("Gabriella has left the hall. Perhaps she is begging food off of people." + newline);
+    					textField.selectAll();
+    				}
+    				break;
+    			case "thomas":
+    				if(!thomasQ) 
+    				{
+    	    			textArea.append(MainHall.getSpecCharacter(toTalk).talkTo() + newline);
+    	    			textField.selectAll();
+    				} else
+    				{
+    					textArea.append("Thomas has left the hall. Maybe he is looking busy to stay alive." + newline);
+    					textField.selectAll();
+    				}
+    				break;
+    			default:
+    				textArea.append("Sorry, but \"" + toTalk + "\" is not in the main hall right now.");
+    				break;
+    			}
     			break;
     		case "ThroneRoom":
     			textArea.append(ThroneRoom.getSpecCharacter(toTalk).talkTo());
@@ -398,29 +477,43 @@ public class KFTextBox extends JPanel implements ActionListener {
         	String accepted = split[1];
         	textField.selectAll();
         	textArea.append(MainHall.getSpecCharacter(accepted).getQuestText() + newline);
-        	System.out.println(accepted);
         	switch(accepted) 
         	{
         	case "bob":
         		curQuest = "bob";
         		questMode = true;
-        		System.out.println(questMode);
         		textArea.append("type \"start\" to start." + newline);
+        		textField.selectAll();
         		break;
         	case "micheal":
         		curQuest = "micheal";
         		questMode = true;
         		textArea.append("type \"start\" to start." + newline);
+        		textField.selectAll();
         		break;
         	case "gabriella":
         		curQuest = "gabriella";
         		questMode = true;
         		textArea.append("type \"start\" to start." + newline);
+        		textField.selectAll();
         		break;
         	case "thomas":
         		curQuest = "thomas";
         		questMode = true;
         		textArea.append("type \"start\" to start." + newline);
+        		textField.selectAll();
+        		break;
+        	case "ragoolaman":
+        		if(finalQA)
+        		{
+        		curQuest = "ragoolaman";
+        		questMode = true;
+        		textArea.append("type \"start\" to start" + newline);
+        		textField.selectAll();
+        		} else
+        		{
+        			textArea.append("You need to finish everything else before you can accept ragoolaman's final quest." + newline);
+        		}
         		break;
         	}
         } else
@@ -460,11 +553,13 @@ public class KFTextBox extends JPanel implements ActionListener {
       		if(order == 2 && text.contains("take"))
       		{
       			textArea.append("You carefully steal the money. The guard doesn't notice! you move with all haste back to bob the locksmith!" + newline);
-      			textArea.append("\"Cheers mate!\"says bob \" I'll get to work on that key now!\"");
-      			textArea.append("Congratulations! Bob is now on your side!");     			
+      			textArea.append("\"Cheers mate!\"says bob \" I'll get to work on that key now!\"" + newline);
+      			textArea.append("Congratulations! Bob is now on your side!" + newline);
+      			textField.selectAll();
       			questMode = false;
       			curQuest = null;
       			initial = false;
+      			bobQ = true;
       		}
     		
     	} else
@@ -481,25 +576,31 @@ public class KFTextBox extends JPanel implements ActionListener {
     		if(text.contains("look around")) 
     		{
     			textArea.append(Dungeon.getItemShortDescs());
+    			textField.selectAll();
     		} else
     	    if (text.contains("examine ")) 
     	    {
     	        	String[] split = text.split(" ");
     	        	examVar = split[1];
-    	        	textArea.append(Dungeon.getSpecItem(examVar).getLongDesc());
+    	        	textField.selectAll();
+    	        	textArea.append(Dungeon.getSpecItem(examVar).getLongDesc() + newline);
     	    } else
     	    if (text.contains("take knife"))     	    	 
     	    {
     	    	textArea.append(Dungeon.itemUsed(Dungeon.KNIFE).itemUsedItem());
+    	    } else 
+    	    if (text.contains("bend bars")){
     	    	textArea.append("You return to micheal, he says:" + newline);
-    	    	textArea.append("Cheers mate, I owe you a biggie now!");
-    	    	textArea.append("Micheal is on your side! Rejoice!");
+    	    	textArea.append("Cheers mate, I owe you a biggie now!" + newline);
+    	    	textArea.append("Micheal is on your side! Rejoice!" + newline);
+    	    	textField.selectAll();
     	    	questMode = false;
     	    	curQuest = null;
     	    	initial = false;
     	    	MainHall.isActive = true;
     	    	Dungeon.isActive = false;
     	    	curRoom();
+    	    	michealQ = true;
     	    }
     	}
     	if(curQuest == "gabriella") 
@@ -514,10 +615,14 @@ public class KFTextBox extends JPanel implements ActionListener {
     			if(text.contains("run")) 
     			{
     				textArea.append("Your quick approach works, you are right behind the guard.");
+    				textField.selectAll();
     			} else
     			if(text.contains("slow") || text.contains("time")) 
     			{
     				textArea.append("As you slowly make your approach, the guard turns! You are caught red handed! The capture, beat, and kill you for your thievery!");
+          			textField.selectAll();
+    				textArea.append(gameOver);
+          			MainMenu.textDemoVisible();
     			} else
     			if(text.contains("take") || text.contains("steal")) 
     			{
@@ -525,9 +630,11 @@ public class KFTextBox extends JPanel implements ActionListener {
     				textArea.append("You give the food to gabriella, and she says:" + newline);
     				textArea.append("Thank you sir! I am ever in your debt!" + newline);
     				textArea.append("Gabriella will now help you when the time is right!" + newline);
+    				textField.selectAll();
     				questMode = false;
     				curQuest = null;
     				initial = false;
+    				gabriellaQ = true;
     			}
     		}
     	}
@@ -535,10 +642,82 @@ public class KFTextBox extends JPanel implements ActionListener {
     	{
     		if(initial == false) 
     		{
-    			textArea.append("You have started micheal's quest, please wait...");
-    			textArea.append("");
+    			textArea.append("You have started thomas' quest, please wait..." + newline);
+    			textArea.append("You see the barrel of mead. The barrel is just to your left. It is heavily guarded." + newline);
+    			initial = true;
+    		} else 
+    		{
+    			if(text.contains("approach") || text.contains("toward")) 
+    			{
+    				textArea.append("As you approach the barrel, a guard confronts you." + newline);
+    				textArea.append("Oi, you there! Yeh think yer gonna get some ale?" + newline);
+    				textArea.append("Do you: talk your way out of it, or do you rush the guard?" + newline);
+    				textField.selectAll();
+    			} else
+    			if(text.contains("rush")) 
+    			{
+    				textArea.append("You rush the guard, and he quickly dispatches you with his dagger. Lifeless, you fall to the ground." + newline);
+    				textArea.append(gameOver);
+    				textField.selectAll();
+          			MainMenu.textDemoVisible();
+    			}
+    			if(text.contains("talk")) 
+    			{
+    				textArea.append("You: I'm getting more for Sir Thomas the Back-Stabbing over there." + newline);
+    				textArea.append("Guard: Very well, but you tell 'im, that he ain't be gettin' anymore t'day!" + newline);
+    				textArea.append("You fill the mug and return it to thomas." + newline);
+    				textArea.append("Thomas: Thanksh mate, I owe ye' for dish one..." + newline);
+    				textField.selectAll();
+    				questMode = false;
+    				curQuest = null;
+    				initial = false;
+    				thomasQ = true;
+    				
+    			}
     		}
     	}
+    	if(curQuest == "ragoolaman") 
+    	{
+    		if(initial == false) 
+    		{
+    			textArea.append("You have started ragoolaman's quest, please wait..." + newline);
+    			textArea.append("As you reach the locked doors to the throne room, you take out the key bob made for you." + newline);
+    			initial = true;
+    		} else {
+    			if(text.contains("unlock") || text.contains("open")) 
+    			{
+    				textArea.append("You insert the key into the lock. As you twist it around, the lock clicks several times. The door swings open suddenly. At the other end of the room, Isratosh sleeps on his throne while someone tries to teach him better english skills." + newline);
+    			} else
+    			if(text.contains("approach")) 
+    			{
+    				textArea.append("As you make your silent approach, you unsheath your sword. The noise attracts the attention of the teacher. \"AHH!\" they yell. \"huh?\" goes the King as he stirs from his fitful sleep." + newline);
+    				textArea.append("The teacher slowly backs away then runs out the door you entered from. \n\"How did you get in here?\" demands the King. As he notices your drawn sword he yells\"GUARDS! TO YOUR KING!\" but no one answers." + newline);
+    				textArea.append("\"You cannot count on your armies tonight Isratosh!\" You say. As you advance towards the King, he cowers on his throne. He mumbles unintelligently as his arms cover his face." + newline);
+    				textArea.append("Your advance has brought you within arms reach of the King. His babbling has reached a yell. The time to end this conflict is now." + newline);
+    			} else
+    			if(text.contains("kill") || text.contains("sword") || text.contains("end") || text.contains("stab")) 
+    			{
+    				textArea.append("You swing your sword... Wit that final blow, the kings head falls lifelessly to the floor." + newline);
+    				textArea.append("As the head hits the floor, the Prince walks in. He whispers \"He is finally dead... THE KING IS DEAD!\"" + newline);
+    				textArea.append("You have earned your title, Feller of Kingdoms. May your sword stay sharp, and your mind stay strong." + newline);
+    				textArea.append("\nCONGRATULATIONS! You have completed the game! The king Isratosh The Gluttonous has been killed! You will go down in history now!" + newline);
+    				textArea.append("A message from the developers:" + newline);
+    				textArea.append("Thank you for playing our first game, we hope you enjoyed it! We will be producing much better games in the future, but a humble and small beginning is somtimes the best." + newline);
+    				textArea.append("-Our Deepest Thanks," + newline);
+    				textArea.append("Ragoolaman + Isratosh" + newline);
+    				textArea.append(newline + "When you have finished reading, type \"finished\" to return to the main menu!" + newline);
+    			} else
+    			if(text.contains("finished")) 
+    			{
+    				MainMenu.textDemoVisible();
+    			} else 
+    			{
+    				textArea.append("That is not a recognizable response." + newline);
+    			}
+    		}
+    		
+    	}
+    	
     }
     }
     public static void goTo(String i) 
@@ -633,19 +812,6 @@ public class KFTextBox extends JPanel implements ActionListener {
      * event dispatch thread.
      */
     public static void main(String[] args) {
-    	 System.out.println(curRoom);
-		// TODO Auto-generated method stub
-    	/*REMOVE THE FOLLOWING LINES AFTER DEVELOPMENT!!!!!*/
-       	/*REMOVE THE FOLLOWING LINES AFTER DEVELOPMENT!!!!!*/
-       	/*REMOVE THE FOLLOWING LINES AFTER DEVELOPMENT!!!!!*/
-       	/*REMOVE THE FOLLOWING LINES AFTER DEVELOPMENT!!!!!*/
-       	/*REMOVE THE FOLLOWING LINES AFTER DEVELOPMENT!!!!!*/
-        /*REMOVE THE FOLLOWING LINES AFTER DEVELOPMENT!!!!!*/
-    	 /*REMOVE THE FOLLOWING LINES AFTER DEVELOPMENT!!!!!*/
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	createAndShowGUI();
-            }
-        });
+
     }
 }
